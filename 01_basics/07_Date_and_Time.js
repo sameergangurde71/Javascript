fbfabc4c3cd3e03c7dd9e67bf4c0293705e2179e
Sameer Gangurde <165358832+sameergangurde71@users.noreply.github.com>
2024-11-27 16:40:59 +0530

    Create 07_Date_and_Time.js

---
+++ 01_basics/07_Date_and_Time.js
@@ -0,0 +1,22 @@
+//Dates
+
+let myDate = new Date()
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+
+let myCreatednewdate = new Date(2025,7,23)
+// console.log(myCreatednewdate.toDateString());
+
+let myTimestamp = Date.now()
+// console.log(myTimestamp);
+// console.log(myCreatednewdate.getTime());
+
+
+//console.log(Date.now()/1000);// millisecond to seconds conversion
+//console.log(Math.floor(Date.now()/1000)); //remove decimals
+
+
+let  newDate = new Date()
+console.log(newDate.getVarDate());
+
