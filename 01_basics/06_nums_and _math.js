1f51f4bb3cbdc12860d6afc72d9572ff50408e5b
Sameer Gangurde <165358832+sameergangurde71@users.noreply.github.com>
2024-11-27 15:47:50 +0530

    Create 06_nums_and _math.js

---
+++ 01_basics/06_nums_and _math.js
@@ -0,0 +1,42 @@
+const num = 400
+//console.log(typeof num);
+
+const score = new Number(12534)
+// console.log(score.toString().split(''));
+// console.log(typeof score);
+
+
+//console.log(score.toFixed(2));
+const balance = 283.76
+//console.log(balance.toPrecision(4));
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString("en-In"));
+
+//**************+++++++++++++++++++++   MATHS +++++++++++++++************************** */
+
+
+// console.log(Math);
+// console.log(Math.abs(-53));
+
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(6.2));
+// console.log(Math.floor(5.7));
+
+
+// console.log(Math.sqrt(64));
+// console.log(Math.sinh(30));
+
+ console.log(Math.pow(2,4));
+
+// console.log(Math.exp(1));
+
+// console.log(Math.max());
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 5
+const max = 52
+
+//console.log(Math.floor(Math.random() * (max - min + 1)) + min); // imp formula
