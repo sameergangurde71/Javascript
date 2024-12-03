70fa2fcc66da9fe26e66f59e4d362de56825fde8
Sameer Gangurde <165358832+sameergangurde71@users.noreply.github.com>
2024-12-03 16:48:51 +0530

    0.2_Control_flow.js

---
+++ 01_basics/0.2_Control_flow.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+//*** If break statement is not present then its run remaining code
+
+const month = "b"
+
+switch (month) {
+    case "a":
+        console.log("January");
+        break;
+    case "b":
+        console.log("february");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("not a present");
+        
+        break;
+}
