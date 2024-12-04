c9395c126bf5e73a6cfb80e8893ead42070f35ae
Sameer Gangurde <165358832+sameergangurde71@users.noreply.github.com>
2024-12-04 14:11:33 +0530

    0.4_iterations1.js

---
+++ 01_basics/0.4_iterations1.js
@@ -0,0 +1,23 @@
+//while / do - while loops
+
+let index = 0
+while (index <= 20) {
+    //console.log(`Value of index ${index}`);
+    index = index + 2
+}
+
+const myArray = ["Krishna","Gopal","Govind","kanha"]
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value of array is ${myArray[arr]}`);
+    arr =arr + 1
+    
+}
+
+let score = 11
+
+do {
+    console.log(`Value of score is ${score}`);
+    score++;
+    
+} while (score <= 10);
