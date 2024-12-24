1f06e79fed19f32c2d8853ece3bf5df35a3549ad
Sameer Gangurde <165358832+sameergangurde71@users.noreply.github.com>
2024-12-24 14:55:53 +0530

    02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // We are using nodejs not browser
+
+console.log( 3+3)
+console.log("smeer");
+
+let name = "Sameer"
+let age = 20
+let isLoggedIn = true
+let num = null;
+let sum
+// number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => valued is not assigned
+// symbol => unique
+
+//object 
+
+console.log(typeof num);// object
+console.log(typeof sum);//undefined
+
